b8aae20239d850140253b95b3cb497dfcce27ca2
Josefina Mancilla <josefinanoemi4@gmail.com>
2017-11-10 17:16:31 -0600

    Fixed checkWinner function to count clicks for each board, made Winner modal display winner names

--- js/app.js
+++ js/app.js
@@ -1,4 +1,36 @@
 $( () => {
+  /******************************
+        MAIN CARDS
+  *******************************/
+// have 2 image divs
+      // one "back of cards"
+      // one "blank"/"selected" card
+// create 2 arrays
+      //original array with img src urls
+      //empty array
+// when deck div is clicked, select random index url and assign that as the attr src for the "selected card" image
+      // simultaneously create a new img with src as that selected URL and append it to modal #cardsInPlay
+//when "selected" card image is clicked,
+      //open up #cardsInPlay modal
+
+
+const $deck = [];
+const $inPlay = [];
+
+const selectCard = () => {
+  $deck[Math.floor(Math.random() * 56)]
+}
+
+
+
+
+
+
+
+
+
+
+
 
   /******************************
         GENERATE BOARD
@@ -73,7 +105,6 @@ const unclickCard = (event) => {
 const clickCard = (event) => {
   $(event.currentTarget).attr('class', 'overlay');
   $(event.currentTarget).on('click', unclickCard);
-  checkWinner();
 }
 
 
@@ -82,27 +113,42 @@ const clickCard = (event) => {
       CHECKING WINNER
 *******************************/
 
-// loops through every board (loop # determined by players variable) --how to make it check on every board????
+// loops through every board (loop # determined by players variable)
     // loop through using i, and #id number i
-
 // loops through every row and every image in every row to check if hasClass('overlay') (this part much like generating function loops)
 // if returns false, stop
 // if all true, invoke winner function
 
 const checkWinner = () => {
-let $overlay = 0;
+// let $overlay = 0;
+let $winnerNames = "";
+let $numOfWinners = 0;
+console.log($winnerNames);
   for (let i = 1; i <= players; i++){
+    let $overlay = 0; //if equals 16, will determine at least one winner (row 145-150)
+    //LOOPS THRU GRIDS (grid = i)
     let $grid = $(`#${i}`).contents();
+    //SELECTS BOARD OF GRID (below)
     let $board = $grid.eq(0).contents();
     // console.log($board);
     for (let r = 0; r < 4; r++){
+      //LOOPS THRU ROWS OF EACH BOARD
       let $row = $board.eq(r).contents();
       // console.log($row);
       for (let card = 0; card < 4; card++) {
+        //LOOPS THRU CARDS OF EACH ROW
         let $card = $row.eq(card);
         if ($card.hasClass('overlay')) {
           $overlay++
+          // want to store the names of players that have won to display name on winner alert text
+          if ($overlay === 16){
+            $numOfWinners++;
+            let $winner = $grid.eq(1).text();
+            $winnerNames +=$winner + "  ";
+            console.log($winnerNames);
+          }
           continue;
+          //will keep looping until false
         } else {
           console.log('break');
           break;
@@ -110,10 +156,14 @@ let $overlay = 0;
       }
     }
   }
-  if ($overlay === 16){
+  //dispays winner message only after checking ALL boards, IF there is a winner
+  if ($numOfWinners >= 1){
+    $('#winner').text($winnerNames);
     $('#modal-winner1').css('display', 'block');
     $('#reset').on('click', ()=> {
       $('#modal-winner1').css('display', 'none');
+      // location.reload();
+      // do we want to reload or reset parameters to beginning, and if so, how?
     });
   }
 }
@@ -149,6 +199,8 @@ const closeNav= () => {
 $('#submit').on('click', closeStart);
 $('#hearts').on('click', openNav);
 $('#close').on('click', closeNav);
+$('h1').on('click', checkWinner);
+      //eventually change this so it runs when deck of cards is clicked!
 
 
 
